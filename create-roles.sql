f4eda5ccff588054298f50d938027f09fde4eaf9
renan.vilela_einov <renan.vilela@einov.com>
2025-08-13 09:29:02 -0300

    Commit docker file

--- create-roles.sql
+++ create-roles.sql
@@ -2,7 +2,7 @@ DO $$
     DECLARE
         r TEXT;
     BEGIN
-        FOR r IN SELECT unnest(ARRAY['zbx_monitor','indicadores']) LOOP
+        FOR r IN SELECT unnest(ARRAY['zbx_monitor','indicadores', 'pooler']) LOOP
                 IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = r) THEN
                     EXECUTE format('CREATE ROLE %I NOLOGIN', r);
                 END IF;
